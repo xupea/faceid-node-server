91d1da307d905a96082fa1971854e0de494bec50
peter <peter@JAVIS.local>
2018-10-08 14:25:38 +0800

    added error handler

--- index.js
+++ index.js
@@ -18,25 +18,41 @@ function handleRedirect(req, res) {
     headers: {
       "Content-Type": "application/json"
     }
-  }).then(function(result) {
-    const signature = result.data.data.signature;
-    const objSign = {
-      sign: signature,
-      sign_version: "hmac_sha1",
-      return_url: "https://faceid-node-server.herokuapp.com/return",
-      notify_url: "https://faceid-node-server.herokuapp.com/notify",
-      capture_image: 0
-    };
-    axios({
-      method: "post",
-      url: "https://openapi.faceid.com/lite_ocr/v1/get_biz_token",
-      data: objSign
-    }).then(function(result) {
-      console.log("biz_token for face id : " + result.data.biz_token);
-      const biz_token = result.data.biz_token;
-      res.redirect("https://openapi.faceid.com/lite_ocr/v1/do/" + biz_token);
+  })
+    .then(function(result) {
+      const signature = result.data.data.signature;
+      const objSign = {
+        sign: signature,
+        sign_version: "hmac_sha1",
+        return_url: "https://faceid-node-server.herokuapp.com/return",
+        notify_url: "https://faceid-node-server.herokuapp.com/notify",
+        capture_image: 0
+      };
+      axios({
+        method: "post",
+        url: "https://openapi.faceid.com/lite_ocr/v1/get_biz_token",
+        data: objSign
+      })
+        .then(function(result) {
+          console.log("biz_token for face id : " + result.data.biz_token);
+          const biz_token = result.data.biz_token;
+          res.redirect(
+            "https://openapi.faceid.com/lite_ocr/v1/do/" + biz_token
+          );
+        })
+        .error(function(msg) {
+          console.log(
+            "Got error when post https://openapi.faceid.com/lite_ocr/v1/get_biz_token : " +
+              msg
+          );
+        });
+    })
+    .error(function(msg) {
+      console.log(
+        "Got error when get http://120.79.193.99:5000/user_account/v1/user/user_face_sign : " +
+          msg
+      );
     });
-  });
   //   const objSign2 = {
   //     sign:
   //       "G1r9GFAxqpQQ2EUhRT8p3lO/fFhhPXJuRWdlZlBoTmNLTkYxbGtwT1llREJGQ25uNXQ4d2tIJmI9MTUzODY1MTEyNyZjPTE1MzgyOTExMjcmZD0xNjQ2MjcwMzg=",
