ae12c1f28e9833b4ac4f990646fdcdb7ab49556c
peter <peter@JAVIS.local>
2018-10-11 18:00:49 +0800

    added missing axios

--- controllers/ocr/return.js
+++ controllers/ocr/return.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 router.get("/", function(req, res) {
   const biz_token = req.query.biz_token;
